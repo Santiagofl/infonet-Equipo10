5c57d2b0f1d732c2a6a671daf3d59d20e9617df9
eduarchilon <eduarchilon@outlook.es>
2022-11-08 01:55:41 -0300

    vistas nuevas

--- controller/abmController.php
+++ controller/abmController.php
@@ -9,7 +9,7 @@ class AbmController
     private $view;
     private $session;
 
-    public function __construct($productosModel, $edicionModel, $seccionModel, $view,$session)
+    public function __construct($productosModel, $edicionModel, $seccionModel, $view, $session)
     {
         $this->view = $view;
         $this->productoModel = $productosModel;
@@ -20,33 +20,37 @@ class AbmController
 
     public function list()
     {
-
+        $data['usuario'] = $this->session->getCurrentUser() ?? '';
         $data['productos'] = $this->productoModel->getProductos();
         $data['ediciones'] = $this->edicionModel->getEdiciones(2);
-        $this->view->render('abm/abmView.mustache', $data);
+        $this->view->render('abm/alta-productosView.mustache', $data);
     }
 
     public function vistaAltaArticulos()
     {
-        $data['usuario']= $this->session->getCurrentUser();
-        $this->view->render('abm/alta-articulosView.mustache',$data);
+        $data['usuario'] = $this->session->getCurrentUser();
+        $data['ediciones'] = $this->edicionModel->getEdiciones(2);
+        $data['secciones'] = $this->seccionModel->getSecciones();
+        $this->view->render('abm/alta-articulosView.mustache', $data);
     }
 
     public function vistaAltaSecciones()
     {
-        $data['usuario']= $this->session->getCurrentUser();
-        $this->view->render('abm/alta-seccionesView.mustache',$data);
+        $data['usuario'] = $this->session->getCurrentUser();
+        $data['ediciones'] = $this->edicionModel->getEdiciones(2);
+        $data['productos'] = $this->productoModel->getProductos();
+        $this->view->render('abm/alta-seccionesView.mustache', $data);
     }
 
     public function vistaAltaProductos()
     {
-        $data['usuario']= $this->session->getCurrentUser();
-        $this->view->render('abm/alta-productosView.mustache',$data);
+        $data['usuario'] = $this->session->getCurrentUser();
+        $this->view->render('abm/alta-productosView.mustache', $data);
     }
 
     public function vistaAltaEdiciones()
     {
-        $data['usuario']= $this->session->getCurrentUser();
+        $data['usuario'] = $this->session->getCurrentUser();
         $data['productos'] = $this->productoModel->getProductos();
         $this->view->render('abm/alta-edicionesView.mustache', $data);
     }
@@ -54,14 +58,14 @@ class AbmController
     //Listas
     public function vistaListaProductos()
     {
-        $data['usuario']= $this->session->getCurrentUser();
+        $data['usuario'] = $this->session->getCurrentUser();
         $data['productos'] = $this->productoModel->getProductos();
         $this->view->render('abm/lista-productosView.mustache', $data);
     }
 
     public function vistaListaSecciones()
     {
-        $data['usuario']= $this->session->getCurrentUser();
+        $data['usuario'] = $this->session->getCurrentUser();
         $data['secciones'] = $this->seccionModel->getSecciones();
         $this->view->render('abm/lista-seccionesView.mustache', $data);
     }
