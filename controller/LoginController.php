d4e6680cd5d12e913e1a578eb7591d0876c0cb72
Santiagofl <sfernandezlopes@unlam.edu.ar>
2022-11-20 18:17:26 -0300

    Adicion md5

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -20,7 +20,7 @@ class LoginController{
             ValidatorHelper::validarSeteadoYNoVacio($_POST["password"])) {
             $usuario = $_POST["usuario"];
             $password = $_POST["password"];
-            $obj = $this->loginModel->iniciarSesion($usuario, $password);
+            $obj = $this->loginModel->iniciarSesion($usuario, md5($password));
 
             if (!empty($obj)) {
                 if($obj["activo"] == 1){
