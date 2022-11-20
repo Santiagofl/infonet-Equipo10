9fa2d99e29bf1de38484620da0f78c6fd2400a6a
eduarchilon <eduarchilon@outlook.es>
2022-11-20 15:22:55 -0300

    estado en model

--- model/ArticuloModel.php
+++ model/ArticuloModel.php
@@ -17,7 +17,8 @@ class ArticuloModel
 
     public function getArticuloPorId($id)
     {
-        $sql = "SELECT * FROM articulo WHERE id_articulo=" . $id;
+        $sql = "SELECT * FROM articulo WHERE id_articulo='$id'
+                AND id_estado=2";
         return $this->database->query($sql);
     }
 
@@ -31,7 +32,7 @@ class ArticuloModel
     {
         $sql = "SELECT * FROM articulo a 
                 JOIN seccion s ON s.id_seccion=a.id_seccion
-                WHERE a.id_seccion =" . $idSeccion;
+                WHERE a.id_seccion ='$idSeccion' AND a.id_estado=2";
         return $this->database->query($sql);
     }
 
