17b5f1f7d3b6be6a129cc58489fa7a0d901a712f
MonteagudoRodrigo <71356504+MonteagudoRodrigo@users.noreply.github.com>
2022-11-21 17:39:21 -0300

    creadas las vistas

--- controller/usuarioController.php
+++ controller/usuarioController.php
@@ -14,4 +14,65 @@ class usuarioController
 
     }
 
+    public function list(){
+
+        if ($this->session->getRol() == 1) {
+
+            $this->view->render('usuariosView.mustache');
+        } else {
+            Redirect::doIt("/infonet/producto");
+        }
+    }
+
+    public function altaUsuario(){
+        if ($this->session->getRol() == 1) {
+            $data["roles"] = $this->usuarioModel->getRoles();
+            $this->view->render('alta-usuarioView.mustache',$data);
+        } else {
+            Redirect::doIt("/infonet/producto");
+        }
+    }
+
+    public function darAltaUsuario(){
+        if(ValidatorHelper::validarSeteadoYNoVacio($_POST["nombre"])&&
+            ValidatorHelper::validarSeteadoYNoVacio($_POST["usuario"])&&
+            ValidatorHelper::validarSeteadoYNoVacio($_POST["clave"])&&
+            ValidatorHelper::validarSeteadoYNoVacio($_POST["email"])&&
+            ValidatorHelper::validarSeteadoYNoVacio($_POST["rol"])) {
+
+            $name = $_POST["nombre"];
+            $user = $_POST["usuario"];
+            $pass = $_POST["clave"];
+            $email = $_POST["email"];
+            $rol = $_POST["rol"];
+            $activo = 1;
+
+            $status = $this->registroModel->registrar($name,$user,$pass,$email,$rol);
+
+            if($status == "existente"){
+                $this->duplicate();
+            }else{
+                Redirect::doIt("/infonet/verificacion");
+            }
+
+        }else{
+            $this->incorrectFormat();
+        }
+
+    }
+
+    public function listaUsuarios(){
+        if ($this->session->getRol() == 1) {
+            $data["usuarios"] = $this->usuarioModel->getUsuarios();
+            $data["roles"] = $this->usuarioModel->getRoles();
+            $this->view->render('lista-usuariosView.mustache',$data);
+        } else {
+            Redirect::doIt("/infonet/producto");
+        }
+
+    }
+
+
+
+
 }
--- model/UsuarioModel.php
+++ model/UsuarioModel.php
@@ -8,4 +8,24 @@ class UsuarioModel
     {
         $this->database = $database;
     }
+
+    public function getUsuarios(){
+        $sql ="SELECT * FROM usuario";
+        return $this->database->query($sql);
+    }
+
+    public function setUsuarios($id_usuario,$rol){
+        $sql = "UPDATE usuario
+        SET rol = '$rol',
+            activo = 1 ,
+            WHERE id_usuario = '$id_usuario'";
+
+        $this->database->execute($sql);
+    }
+
+    public function getRoles(){
+        $sql ="SELECT * FROM rol";
+        return $this->database->query($sql);
+    }
+
 }
