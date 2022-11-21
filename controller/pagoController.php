942dce5eef70a48b9c1febaa45fd449081fad459
Santiagofl <sfernandezlopes@unlam.edu.ar>
2022-11-21 19:57:59 -0300

    Fix session compra/suscripcion

--- controller/pagoController.php
+++ controller/pagoController.php
@@ -40,75 +40,84 @@ class pagoController{
     }
 
     public function comprarEdicion(){
-        $edicion = $_GET["edicion"];
-        $user = $_GET["usuario"];
 
-        $ediciones= $this->edicionModel->getEdicionById($edicion);
-        require 'vendor/autoload.php';
+        if ($this->session->getCurrentUser() != 0) {
+            $edicion = $_GET["edicion"];
+            $user = $_GET["usuario"];
 
-        MercadoPago\SDK::setAccessToken('TEST-5322929221556591-110901-5614eabea6ba44256133cc0858a47ebb-328479679');
+            $ediciones = $this->edicionModel->getEdicionById($edicion);
+            require 'vendor/autoload.php';
 
-        $preference = new MercadoPago\Preference();
+            MercadoPago\SDK::setAccessToken('TEST-5322929221556591-110901-5614eabea6ba44256133cc0858a47ebb-328479679');
 
-        $item = new MercadoPago\Item();
+            $preference = new MercadoPago\Preference();
 
-        $item->title = $ediciones[0]['evento'];
-        $item->quantity = 1;
-        $item->unit_price = $ediciones[0]['precio'];
-        $item->currency_id = "ARS";
+            $item = new MercadoPago\Item();
 
-        $preference->items = array($item);
+            $item->title = $ediciones[0]['evento'];
+            $item->quantity = 1;
+            $item->unit_price = $ediciones[0]['precio'];
+            $item->currency_id = "ARS";
 
-        $preference->back_urls = array(
-            "success" => "http://localhost/infonet/pago/pagoConfirmado?usuario=$user&edicion=$edicion",
-            "failure" => "http://localhost/infonet/pago/pagoFallido",
-        );
+            $preference->items = array($item);
 
-        $preference->auto_return = "approved";
-        $preference->binary_mode = true;
+            $preference->back_urls = array(
+                "success" => "http://localhost/infonet/pago/pagoConfirmado?usuario=$user&edicion=$edicion",
+                "failure" => "http://localhost/infonet/pago/pagoFallido",
+            );
 
-        $preference->save();
+            $preference->auto_return = "approved";
+            $preference->binary_mode = true;
 
-        $data['preferencias'] = $preference;
+            $preference->save();
 
-        $this->list($data);
+            $data['preferencias'] = $preference;
+
+            $this->list($data);
+        }else{
+            Redirect::doIt('/infonet/login');
+        }
     }
 
     public function suscribirse(){
 
-        $id_producto = $_GET['id_producto'];
-        $id_usuario = $_GET['id_usuario'];
+        if ($this->session->getCurrentUser() != 0) {
+            $id_producto = $_GET['id_producto'];
+            $id_usuario = $_GET['id_usuario'];
 
-        $producto = $this->productoModel->getProducto($id_producto);
+            $producto = $this->productoModel->getProducto($id_producto);
 
-        require 'vendor/autoload.php';
+            require 'vendor/autoload.php';
 
-        MercadoPago\SDK::setAccessToken('TEST-5322929221556591-110901-5614eabea6ba44256133cc0858a47ebb-328479679');
+            MercadoPago\SDK::setAccessToken('TEST-5322929221556591-110901-5614eabea6ba44256133cc0858a47ebb-328479679');
 
-        $preference = new MercadoPago\Preference();
+            $preference = new MercadoPago\Preference();
 
-        $item = new MercadoPago\Item();
+            $item = new MercadoPago\Item();
 
-        $item->title = $producto[0]['nombre'];
-        $item->quantity = 1;
-        $item->unit_price = $producto[0]['precio'];
-        $item->currency_id = "ARS";
+            $item->title = $producto[0]['nombre'];
+            $item->quantity = 1;
+            $item->unit_price = $producto[0]['precio'];
+            $item->currency_id = "ARS";
 
-        $preference->items = array($item);
+            $preference->items = array($item);
 
-        $preference->back_urls = array(
-            "success" => "http://localhost/infonet/pago/pagoConfirmado?usuario=$id_usuario&id_producto=$id_producto",
-            "failure" => "http://localhost/infonet/pago/pagoFallido",
-        );
+            $preference->back_urls = array(
+                "success" => "http://localhost/infonet/pago/pagoConfirmado?usuario=$id_usuario&id_producto=$id_producto",
+                "failure" => "http://localhost/infonet/pago/pagoFallido",
+            );
 
-        $preference->auto_return = "approved";
-        $preference->binary_mode = true;
+            $preference->auto_return = "approved";
+            $preference->binary_mode = true;
 
-        $preference->save();
+            $preference->save();
 
-        $data['preferencias'] = $preference;
+            $data['preferencias'] = $preference;
 
-        $this->list($data);
+            $this->list($data);
+        }else{
+            Redirect::doIt('/infonet/login');
+        }
     }
 
 }
