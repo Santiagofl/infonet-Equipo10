64dd8f49a4f9ff34d95920574fd5b59ba79407c4
eduarchilon <eduarchilon@outlook.es>
2022-11-22 12:59:24 -0300

    fix style

--- helpers/PdfManager.php
+++ helpers/PdfManager.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'dependencies/dompdf/autoload.inc.php';
+
 use Dompdf\Dompdf;
 
 class PdfManager
@@ -12,8 +13,9 @@ class PdfManager
 
     public function createPdf($usuarioId, $ediciones, $esDescarga)
     {
-        $html = "<h4 style='text-align:center;'>Mis Ediciones</h4>";
-        $html .= "<table border='1' width='100%' style='border-collapse: collapse;'>
+        $html = "<span><span class='info'>Info</span><span class='net'>net</span></span>
+<h4 class='title-table'>Mis Ediciones</h4>";
+        $html .= "<table border='1' width='100%' style='border-collapse: collapse';>
                     <tr>
                         <th>Id Edición</th>
                         <th>Fecha</th>
@@ -22,17 +24,65 @@ class PdfManager
                         
                     </tr>";
 
-        for($i = 0; $i < sizeof($ediciones); $i++){
+        for ($i = 0; $i < sizeof($ediciones); $i++) {
             $html .=
-                    "<tr>
-                        <td>".$ediciones[$i]['id_edicion']."</td>
-                        <td>".$ediciones[$i]['fecha']."</td>
-                        <td>".$ediciones[$i]['nombre']."</td>
-                        <td>".$ediciones[$i]['evento']."</td>
+                "<tr>
+                        <td>" . $ediciones[$i]['id_edicion'] . "</td>
+                        <td>" . $ediciones[$i]['fecha'] . "</td>
+                        <td>" . $ediciones[$i]['nombre'] . "</td>
+                        <td>" . $ediciones[$i]['evento'] . "</td>
                     </tr>";
         }
 
         $html .= "</table>";
+        $html .= "
+<style>
+                    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+                    .title-table{
+                    font-size: 45px;
+                    }
+                    
+                    .info{
+                    background-color: green;
+                    color: #fff;
+                    padding: 5px 5px; 
+                    border-radius: 5px;
+                    margin-right: 2px;
+                    }
+                    table, .info, .net, .title-table{
+                    font-family: 'Poppins', sans-serif;
+                    }
+                    
+                    .info, .net{
+                    font-size: 30px; 
+                    }
+                    .title-table{
+                    font-size: 30px;
+                    }
+                    
+                    td, th{
+                    padding: 10px 0; 
+                    text-align: center; 
+                    }
+                    
+                    th:nth-child(1){
+                    background-color: red;
+                    color: #ffff;
+                    }
+                    th:nth-child(2){
+                    background-color: green;
+                    color: #ffff;
+                    }
+                    th:nth-child(3){
+                    background-color: black;
+                    color: #ffff;
+                    }
+                    th:nth-child(4){
+                    background-color: blue;
+                    color: #ffff;
+                    }
+                    </style>";
+
 
         $filename = "MisEdiciones";
 
@@ -42,10 +92,10 @@ class PdfManager
         $dompdf->loadHtml($html);
         $dompdf->setPaper('A4', 'landscape');
         $dompdf->render();
-        if($esDescarga === true){
-            $dompdf->stream($filename,array("Attachment"=>1));
+        if ($esDescarga === true) {
+            $dompdf->stream($filename, array("Attachment" => 1));
         } else {
-            $dompdf->stream($filename,array("Attachment"=>0));
+            $dompdf->stream($filename, array("Attachment" => 0));
         }
 
         //}catch (Exception $e){var_dump($e->getMessage());}
